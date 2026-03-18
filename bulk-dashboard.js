e95860d1438e5b6e97a1a6980b1d1316fd807fe4
kawalekailas-code <kawale.kailas@gmail.com>
2026-03-18 14:57:39 +0530

    Add files via upload

---
+++ bulk-dashboard.js
@@ -0,0 +1,27 @@
+
+let successCount = 0;
+let failCount = 0;
+
+function updateStats() {
+  document.getElementById('successCount').innerText = successCount;
+  document.getElementById('failCount').innerText = failCount;
+}
+
+function addError(message) {
+  failCount++;
+  updateStats();
+
+  const table = document.querySelector('#errorTable tbody');
+  const row = document.createElement('tr');
+  row.innerHTML = `<td>${new Date().toLocaleTimeString()}</td><td style="word-break:break-word;">${message}</td>`;
+  table.prepend(row);
+
+  if (table.children.length > 50) {
+    table.removeChild(table.lastChild);
+  }
+}
+
+function addSuccess() {
+  successCount++;
+  updateStats();
+}
